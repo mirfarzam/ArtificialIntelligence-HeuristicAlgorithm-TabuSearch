17fc755770b2144c5f678adf59ad5d930c92f5ca
mirfarzam <farzam.mirmoeini@gmail.com>
2020-04-17 22:51:35 +0200

    change python file name

--- tester.py
+++ tester.py
@@ -3,69 +3,62 @@ import subprocess
 import re
 from datetime import datetime
 import time
+from statistics import mean
+import csv
 
 
-# Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
-    # Print New Line on Complete
-    if iteration == total:
-        print()
+numberOfTests = 10
 
-class Route():
-    def __init__(self, route, distance, time):
-        self.route = route
-        self.distance = distance
-        self.time = time
+tabuIteration = '100'
+tabuDuration = '40'
+numberOfCities = '10'
+alphaTabu = 1
+constantTabu = 10
 
-routes = []
+final_solution = []
+list_coverage = []
+local_minimum = []
 
-# printProgressBar(0, 10, prefix = 'Progress:', suffix = 'Complete', length = 50)
-for i in range(0, 5):
-    startTime = datetime.now()
-    process = subprocess.Popen(['./algo_tabou.exe', '1000', '1000', '50', 'distances_entre_villes_50.txt'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+headerCSV = [i+1 for i in range(int(numberOfCities))]
+rowList = []
+rowList.append(headerCSV)
+
+print(f"\n\nTest for Tabu Search with this config: \n\tIterations : {tabuIteration} \n\tNumber of Cities: {numberOfCities} \n\tAlpha For Tabu Duration = {alphaTabu} \n\tConstant For Tabu Duration = {constantTabu}")
+for i in range(0, numberOfTests):
+    process = subprocess.Popen(['./algo_tabou.exe', tabuIteration , tabuDuration, numberOfCities, 'distances_entre_villes_{}.txt'.format(numberOfCities)],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    # print(stdout)
-    endTime = datetime.now() - startTime
-    result = re.findall(r'[0-9\-]+\-\-> [0-9]{4}', str(stdout))
-    bestRoute = (list(map(lambda x: re.sub('\-{3}>', '', x), result))[1]).split(" ")
-    print(bestRoute)
-    routes.append(Route(bestRoute[0], int(bestRoute[1]), endTime.microseconds))
-    process.terminate()
+    result = stdout
+    result = re.sub(r'\s', ' ', str(result))
+    solution = (re.findall(r'([0-9]{4,7}) km', result))[-1]
+    meanDuration = (re.findall(r'Mean Duration : ([0-9]+\.[0-9]+)', result))[0]
+    meanDurationPerCity = re.findall(r'MDPC([0-9]+\.[0-9]+),', result)
+    rowList.append([float(i) for i in meanDurationPerCity])
+    final_solution.append(int(solution))
+    coverage = re.findall(r'On est dans un minimum local a l\'iteration ([0-9]+) ->', result)
+    if coverage != []:
+        coverage = int(coverage[0])+ 1
+        if int(solution) != 5644:
+            local_minimum.append(int(solution))
+    else:
+        coverage = int(tabuIteration)
+    number_of_solution_before_coverage = coverage
+    list_coverage.append(coverage)
+    print('{} : best found solution is {} and found in interation {}, number of solutions before coverage : {}, mean duration was : {}'.format(i, solution, coverage, number_of_solution_before_coverage, meanDuration))
     time.sleep( 1 )
-    # printProgressBar(i + 1, 10, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
-# Routes Information
-justRoutes = list(map(lambda x : x.route, routes))
-uniqueRoutes = len(list(dict.fromkeys(justRoutes)))
-print("Number of unique Routes : {}".format(uniqueRoutes))
 
-# Distance Information
-distances = list(map(lambda x : x.distance, routes))
-minimumDistance = min(distances)
-AverageDistance = sum(distances) / len(distances)
-print("Minimum Distance : {}".format(minimumDistance))
-print("Average Distance : {}".format(AverageDistance))
+with open(f'{numberOfCities}-{tabuIteration}-{alphaTabu}-{constantTabu}.csv', 'w', newline='') as file:
+    writer = csv.writer(file,delimiter=',')
+    writer.writerows(rowList)
 
-# Time Consuming Information
-times = list(map(lambda x : x.time, routes))
-AverageTimes = sum(times) / len(times)
-print("Average Time it took to find a best route : {} microseconds".format(AverageTimes))
+print("Summary:")
 
-sorted_routes = sorted(routes, key=lambda student: student.distance)
-print(f"best route is {sorted_routes[0].route} and distance is {sorted_routes[0].distance} and takes {sorted_routes[0].time} microseconds")
+optimum_result = len(list(filter(lambda x: x == 5644, final_solution)))
+
+print(f'number of optimum solution found is {optimum_result}, so in {numberOfTests} runs of test we faced {(optimum_result/numberOfTests)*100}% coverage')
+print(f'in average this test shows that we found the global optimum solution in iteration {mean(list_coverage)}\nand in worst we found it in iteration {max(list_coverage)} \nand in best case in iteration {max(list_coverage)}')
+print(f'Totally, {sum(list_coverage)} cities visited before finding the global optimum in {numberOfTests} runs of this test\n')
+
+unique_local_minimum = list(dict.fromkeys(local_minimum))
+
+print(f'totally the algorithm was stuck in local optimum {len(local_minimum)} times \nthere are {len(unique_local_minimum)} unique local minimum \nthe best local optimum is {min(unique_local_minimum)} \nthe worst local optimum is {max(unique_local_minimum)}')
